85aa36eb70ac53cc619abdae948f58b47154abea
Taehun92 <taehun1218@gmail.com>
2025-06-11 18:20:19 +0900

    2025.06.11 재료 DB 등록 완료 , 마이바 페이지 - 재료 탭 구성

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,17 @@ import 'common/myPage.dart';
 import 'common/myRecipe.dart';
 import 'common/wishList.dart';
 import 'common/buyProduct.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
