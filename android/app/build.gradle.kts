6c0a857be769a1d5fdc035a70ec8e0f17c4264e8
yulyuling <egoist.ly@gmail.com>
2025-06-17 12:57:48 +0900

    2025.06.17 카톡 로그인 로그아웃 성공

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.arcohol"
+    namespace = "com.flutter.arcohol"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.arcohol"
+        applicationId = "com.flutter.arcohol"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
